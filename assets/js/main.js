fbd2ad8ffecdc9a9f1f55cc4e0c6b6d074177f46
muhamad fais aizat <128875374+muhamadaizat@users.noreply.github.com>
2023-10-23 08:30:48 +0700

    commit admin js

--- assets/js/main.js
+++ assets/js/main.js
@@ -12,10 +12,35 @@ list.forEach((item) => item.addEventListener("mouseover", activeLink));
 
 // Menu Toggle
 let toggle = document.querySelector(".toggle");
+let toggle2 = document.querySelector(".toggle2");
 let navigation = document.querySelector(".navigation");
 let main = document.querySelector(".main");
+let submenu = document.querySelector(".submenu");
 
 toggle.onclick = function () {
   navigation.classList.toggle("active");
   main.classList.toggle("active");
+  submenu.classList.toggle("active");
 };
+toggle2.onclick = function () {
+  navigation.classList.toggle2("active");
+  main.classList.toggle2("active");
+  submenu.classList.toggle2("active");
+};
+
+// function myFunction() {
+//   document.getElementById("myDropdown").classList.toggle("show");
+// }
+
+// window.onclick = function(event) {
+//   if (!event.target.matches('.submenu')) {
+//     var dropdowns = document.getElementsByClassName("sub-menu");
+//     var i;
+//     for (i = 0; i < dropdowns.length; i++) {
+//       var openDropdown = dropdowns[i];
+//       if (openDropdown.classList.contains('show')) {
+//         openDropdown.classList.remove('show');
+//       }
+//     }
+//   }
+// }

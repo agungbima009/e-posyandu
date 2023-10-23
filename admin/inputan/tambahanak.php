a2fa012571d09d3c3d6dc6f340627a14c6225a1f
muhamad fais aizat <128875374+muhamadaizat@users.noreply.github.com>
2023-10-24 00:03:31 +0700

    commit inputan

---
+++ admin/inputan/tambahanak.php
@@ -0,0 +1,56 @@
+<!-- <link href="https://unpkg.com/tailwindcss@^1.0/dist/tailwind.min.css" rel="stylesheet"> -->
+<link rel="stylesheet" href="/assets/css/tabeladmin.css">
+<div class="bg-input">
+    <div class="jd-input">
+        <h1 style="color: #24297a;">Tambah Data Anak</h1>
+    </div>
+    <div class=" box-input">
+        <p>Nama Anak</p>
+        <div class="bg-input-1">
+            <input class="input-ds-1" type="text" name="username" placeholder="nama_anak...." />
+        </div>
+    </div>
+    <div class=" box-input">
+        <p>NIK Anak</p>
+        <div class="bg-input-1">
+            <input class="input-ds-1" type="text" name="username" placeholder="masukan NIK..." />
+        </div>
+    </div>
+    <div class=" box-input-vertical">
+        <div class=" box-input">
+            <p>Tempat lahir</p>
+            <div class="bg-input-1">
+                <input class="input-ds-1" type="text" name="username" placeholder="tempat..." />
+            </div>
+        </div>
+        <div class=" box-input">
+            <p>Tanggal lahir</p>
+            <div class="bg-input-1">
+                <input class="input-ds-1" type="date" name="username" placeholder="tempat..." />
+            </div>
+        </div>
+    </div>
+    <div class=" box-input">
+        <p>Usia Anak</p>
+        <div class="bg-input-1">
+            <input class="input-ds-1" type="text" name="username" placeholder="masukan usia anak..." />
+        </div>
+    </div>
+    <div class=" box-input">
+        <p>Jenis kelamnin</p>
+        <div class="box-input-vertical-1">
+            <p><input class="" type="radio" value="Laki-Laki" name="username" /> Laki-Laki</p>
+            <p><input class="" type="radio" value="Perempuan" name="username" /> Perempuan</p>
+        </div>
+    </div>
+    <div class=" box-input">
+        <div class="box-input-vertical" style="padding-bottom: 5px;">
+            <p>Nama Ibu</p>
+            <button class="button-bg-green-1" type="button">tambah</button>
+        </div>
+        <input class="input-ds-1" type="text" name="username" placeholder="masukan usia anak..." />
+    </div>
+    <div class="p-kanan" style="padding-top: 10px;">
+        <button class="button-bg-1" type="button">Simpan</button>
+    </div>
+</div>

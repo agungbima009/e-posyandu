2f9f019f70c25f64064707736f10d14bcc1f0018
muhamad fais aizat <128875374+muhamadaizat@users.noreply.github.com>
2023-10-24 09:28:21 +0700

    assets

--- assets/js/main.js
+++ assets/js/main.js
@@ -38,10 +38,69 @@ function toggleContent(menuId) {
   selectedContent.style.display = 'block';
 }
 
+// form anak
 document.getElementById('openPopup').addEventListener('click', function() {
   document.getElementById('popup').style.display = 'block';
 });
 
 document.getElementById('closePopup').addEventListener('click', function() {
   document.getElementById('popup').style.display = 'none';
+});
+
+// form ibu
+document.addEventListener('DOMContentLoaded', function() {
+  document.getElementById('openPopupibu').addEventListener('click', function() {
+    document.getElementById('popupibu').style.display = 'block';
+  });
+
+  document.getElementById('closePopupibu').addEventListener('click', function() {
+    document.getElementById('popupibu').style.display = 'none';
+  });
+});
+
+
+// box cari 
+document.addEventListener('DOMContentLoaded', function() {
+const wrapper = document.querySelector(".wrapper-box-cari"),
+selectBtn = wrapper.querySelector(".select-btn-cari"),
+searchInp = wrapper.querySelector("input"),
+options = wrapper.querySelector(".options-cari");
+
+let countries = ["Afghanistan", "Algeria", "Argentina", "Australia", "Bangladesh", "Belgium", "Bhutan",
+                 "Brazil", "Canada", "China", "Denmark", "Ethiopia", "Finland", "France", "Germany",
+                 "Hungary", "Iceland", "India", "Indonesia", "Iran", "Italy", "Japan", "Malaysia",
+                 "Maldives", "Mexico", "Morocco", "Nepal", "Netherlands", "Nigeria", "Norway", "Pakistan",
+                 "Peru", "Russia", "Romania", "South Africa", "Spain", "Sri Lanka", "Sweden", "Switzerland",
+                 "Thailand", "Turkey", "Uganda", "Ukraine", "United States", "United Kingdom", "Vietnam"];
+
+function addCountry(selectedCountry) {
+    options.innerHTML = "";
+    countries.forEach(country => {
+        let isSelected = country == selectedCountry ? "selected" : "";
+        let li = `<li onclick="updateName(this)" class="${isSelected}">${country}</li>`;
+        options.insertAdjacentHTML("beforeend", li);
+    });
+}
+addCountry();
+
+function updateName(selectedLi) {
+    searchInp.value = "";
+    addCountry(selectedLi.innerText);
+    wrapper.classList.remove("active");
+    selectBtn.firstElementChild.innerText = selectedLi.innerText;
+}
+
+searchInp.addEventListener("keyup", () => {
+    let arr = [];
+    let searchWord = searchInp.value.toLowerCase();
+    arr = countries.filter(data => {
+        return data.toLowerCase().startsWith(searchWord);
+    }).map(data => {
+        let isSelected = data == selectBtn.firstElementChild.innerText ? "selected" : "";
+        return `<li onclick="updateName(this)" class="${isSelected}">${data}</li>`;
+    }).join("");
+    options.innerHTML = arr ? arr : `<p style="margin-top: 10px;">Oops! Country not found</p>`;
+});
+
+selectBtn.addEventListener("click", () => wrapper.classList.toggle("active")); 
 });
